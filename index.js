f5e5e69b2f47cd24d669decef255d0c5aa1d3e7f
Ananya Dhingra <ananyadhingra6@gmail.com>
2021-06-02 22:58:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ exports.handler = (event,context) => {
     // YOU CAN CHANGE THE BELOW URL DEPENDING ON YOUR CITY
     //To get stated id : https://cdn-api.co-vin.in/api/v2/admin/location/states, 
     // Put state ID in link https://cdn-api.co-vin.in/api/v2/admin/location/districts/: PUT this district id in [DISTRICT_ID] below
-    //var south_delhi_url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=[DISTRICT_ID]&date="+today_date
+    //var city_url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=[DISTRICT_ID]&date="+today_date
 
     var ggn_url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=188&date="+today_date
     hitApi.hit(ggn_url,"Gurgaon")
 
+    //Call your city url by uncommenting below:
+    //hitApi.hit(city_url,"City")
+    
 };
     
 

1b4ad27ce0da4c140d9d0a5091d84830fba12c67
Ananya Dhingra <ananyadhingra6@gmail.com>
2021-06-02 22:57:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,79 +9,14 @@ exports.handler = (event,context) => {
     var mm = String(today.getMonth() + 1).padStart(2, '0');
     var today_date=dd+"-"+mm+"-2021";
     
-    
+    // YOU CAN CHANGE THE BELOW URL DEPENDING ON YOUR CITY
+    //To get stated id : https://cdn-api.co-vin.in/api/v2/admin/location/states, 
+    // Put state ID in link https://cdn-api.co-vin.in/api/v2/admin/location/districts/: PUT this district id in [DISTRICT_ID] below
+    //var south_delhi_url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=[DISTRICT_ID]&date="+today_date
+
     var ggn_url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=188&date="+today_date
-    var south_delhi_url="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=149&date="+today_date
-
     hitApi.hit(ggn_url,"Gurgaon")
-    //hitApi.hit(south_delhi_url,"Delhi")
 
-//   const req = https.get(ggn_url,
-//         (res)=> {
-//           res.on('data', chunk => {
-//             dataString += chunk;
-//           });
-          
-//           res.on('end', () => {
-//             console.log("ending")
-//             var allVac=JSON.parse(dataString,null,2)
-//             var i=0
-//               //console.log(allVac);
-          
-//             //console.log(allVac.centers[0].sessions[0]);
-            
-//             var avl_slots=[]
-//             allVac.centers.forEach((center)=> {
-                
-//                 var name=center.name;
-//                 center.sessions.forEach(session=>{
-//                   //  console.log("min_age_limit: ",session.min_age_limit);
-//                     var age=session.min_age_limit;
-//                     var date=session.date
-//                     var vaccine=session.vaccine;
-//                     var slots=session.slots;
-//                     var avl_dose1=session.available_capacity_dose1;
-//                     var avl_dose2=session.available_capacity_dose2;
-                   
-//                   if(vaccine=='COVAXIN'){
-//                     if(age==18){
-//                       var avl_json={
-//                          "No": i,
-//                          "Name":name,
-//                          "Date":date,
-//                          "Time Slots":slots,
-//                          "Qty":avl_dose1}
-                      
-                       
-//                       var avl=JSON.stringify(avl_json)
-//                       //console.log(avl_details)
-//                       // avl_slots.push(avl)
-                       
-//                       var mySlot=i+" Center Name: "+name+" Date: "+date+" Time Slots: "+slots+" Qty: "+avl_dose1+"--";
-//                       i++;
-//                         //avl_slots+="------SLOT------------\n"+mySlot
-//                       avl_slots.push(mySlot)
-                        
-//                       }
-//                   }
-//                 })
-//              });
-
-            
-//           avl_slots=avl_slots.toString()
-//           var check_string="-----SLOT------------\nhey" 
-//           //mssg.handler(avl_slots)
-                  
-//           })
-        
-//         }).on('error', (e) => {
-//           reject({
-//               statusCode: 500,
-//               body: 'Something went wrong!'
-//           });
-//         });
 };
     
-    
-    //return response;
 
